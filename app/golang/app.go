32967f334d25bb6f4b48d19a3817548d9743a219
mazrean <shunsuke.w04@gmail.com>
2022-07-12 23:04:40 +0900

    fix img

--- app/golang/app.go
+++ app/golang/app.go
@@ -790,7 +790,7 @@ func getImage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	post := Post{}
-	err = db.Get(&post, "SELECT `imgdata` FROM `posts` WHERE `id` = ?", pid)
+	err = db.Get(&post, "SELECT `mime`, `imgdata` FROM `posts` WHERE `id` = ?", pid)
 	if err != nil {
 		log.Print(err)
 		return
